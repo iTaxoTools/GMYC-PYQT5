5035d26320a1b9edf53b64ffd1e50bc39c27f3b3
sangeeta97 <kumarisangeeta097@gmail.com>
2021-03-16 10:23:26 +0100

    fixed spart bug

--- GMYC.py
+++ GMYC.py
@@ -230,9 +230,10 @@ class um_tree:
 
     def print_species_spart(self, save_file, p_value=None):
 
-        fo_bestpar = open(os.path.join(save_file, self.unique + "partition.spart"), "w+")
+        fo_bestpar = open(os.path.join(save_file, self.unique + "partition_gmyc.spart"), "w+")
         
         if self.fileextension: directory, file= os.path.split(self.fileextension)
+	file= re.sub(r'[^A-Za-z0-9_]', '_', file)
 
         fo_bestpar.write("begin spart;\n\n")
         fo_bestpar.write(f"Project_name = {file};\n")
@@ -254,7 +255,7 @@ class um_tree:
                 xx= yy +":" + str(i+1)
                 ll.append(xx)
 
-        fo_bestpar.writelines([x+"\n" for x in ll[0:-2]])
+        fo_bestpar.writelines([x+"\n" for x in ll[0:-1]])
         fo_bestpar.write(f"{ll[-1]};\n\n")
         fo_bestpar.write(f"Spartition_score_type = Likelihood ratio test p-value;\n")
 
